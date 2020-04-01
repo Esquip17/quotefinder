4105c6f2cd900bbdf6bddbb0e75505a77ca393a7
Norman Calhoun <ndc1963@gmail.com>
2020-04-01 09:19:07 -0400

    models changed.

--- quotefinder_app/models.py
+++ quotefinder_app/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
 
-class Quote(models.Model):
-    body = models.CharField(max_length=300)
-
-    def __str__(self):
-        return self.body
-
-
 class Author(models.Model):
     name = models.CharField(max_length=100)
     photo_url = models.TextField()
 
     def __str__(self):
         return self.name  # Create your models here.
+
+
+class Quote(models.Model):
+    body = models.CharField(max_length=300)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, related_name='quotes', null=True)
+
+    def __str__(self):
+        return self.body
